e39cf3c144ab0022962a740e26aa45c9048543d7
jwhite <jtwhite1000@gmail.com>
2026-01-15 20:10:12 -0700

    more pest stuff

--- exercises/notebooks/helpers.py
+++ exercises/notebooks/helpers.py
@@ -512,11 +512,11 @@ def plot_ies_forecasts(m_d, pst_name="pest.pst", noptmax=None, include_t=False):
 if __name__ == "__main__":
     # process_csv_files(os.path.join("..","models","synthetic-valley-truth-advanced-monthly"))
     # process_csv_files(os.path.join("model_and_pest_files_opt"))
-    # extract_true_obs(
-    #    os.path.join("..", "models", "synthetic-valley-truth-advanced-monthly")
-    # )
+    extract_true_obs(
+        os.path.join("..", "models", "synthetic-valley-truth-advanced-monthly")
+    )
     # fig,axes = plot_ies_properties("master_ies_advanced","sto-ss-layer1",noptmax=None)
     # plt.savefig("test.pdf")
     # plt.close(fig)
-    plot_ies_timeseries("master_ies_base", noptmax=None)
+    # plot_ies_timeseries("master_ies_base", noptmax=None)
     # plot_ies_forecasts("master_ies_advanced", noptmax=None)
